59436fac3a4055ab1332cdb27880a20ccb28dd76
Kevin V <kevin.valette@gmail.com>
2016-09-07 14:40:15 +0000

    modification admin.js

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,88 +1,93 @@
 /* global Vue*/
 /* global io*/
 /* global $*/
-    var socket = io.connect(GLOBAL.url);
-    new Vue({
-        el: '#content',
-        data: {load : true,
-            state: {
-                step : ''
-            },
-            nomPage : 'Paramétrage de votre partie',
-            param : {
-              user : 1,
-              question : 20,
-              timer : 15
-            },
-            nbUsersMax : [
-                { text : '1' },
-                { text : '2' },
-                { text : '3' },
-                { text : '4' }
-            ],
-            nbQuestionsMax : [
-                { text : '5' },
-                { text : '10' },
-                { text : '15' },
-                { text : '20' }
-            ],
-            timerQuestionMax : [
-                { text : '15' },
-                { text : '30' },
-                { text : '45' },
-                { text : '60' }
-            ],
-            form : {
-                msg : "",
-                valid : true
+var socket = io.connect(GLOBAL.url);
+new Vue({
+    el: '#content',
+    data: {
+        loading : false,
+        error : GLOBAL.error,
+        state: {
+            step : ''
+        },
+        nomPage : 'Paramétrage de votre partie',
+        param : {
+          user : 1,
+          question : 20,
+          timer : 15
+        },
+        nbUsersMax : [
+            { text : '1' },
+            { text : '2' },
+            { text : '3' },
+            { text : '4' }
+        ],
+        nbQuestionsMax : [
+            { text : '5' },
+            { text : '10' },
+            { text : '15' },
+            { text : '20' }
+        ],
+        timerQuestionMax : [
+            { text : '15' },
+            { text : '30' },
+            { text : '45' },
+            { text : '60' }
+        ],
+        form : {
+            msg : "",
+            valid : true
+        }
+    },
+    ready: function() {
+        this.loading = true;
+        $("#content").show();
+    },
+    methods: {
+        controleDeSurface : function() {
+            this.form.msg = "";
+            this.form.valid = true;
+            
+            //verif des 3 champs number
+            if (! this.param.user.toString().match(/^[0-9]{1,2}$/)) {
+                this.form.msg = "Veuillez indiquer le nombre de participants.\n";
+                this.form.valid = false;
+            }
+            if(! this.param.question.toString().match(/^[0-9]{1,2}$/)){
+                this.form.msg = "Veuillez indiquer le nombre de questions.\n";
+                this.form.valid = false;
+            }
+            if(! this.param.timer.toString().match(/^[0-9]{1,2}$/)){
+                this.form.msg = "Veuillez indiquer le temps de réponse autorisé.\n";
+                this.form.valid = false;
             }
         },
-        methods: {
-            controleDeSurface : function() {
-                this.form.msg = "";
-                this.form.valid = true;
-                
-                //verif des 3 champs number
-                if (! this.param.user.toString().match(/^[0-9]{1,2}$/)) {
-                    this.form.msg = "Veuillez indiquer le nombre de participants.\n";
-                    this.form.valid = false;
-                }
-                if(! this.param.question.toString().match(/^[0-9]{1,2}$/)){
-                    this.form.msg = "Veuillez indiquer le nombre de questions.\n";
-                    this.form.valid = false;
-                }
-                if(! this.param.timer.toString().match(/^[0-9]{1,2}$/)){
-                    this.form.msg = "Veuillez indiquer le temps de réponse autorisé.\n";
-                    this.form.valid = false;
-                }
-            },
+        
+        createRoom : function() {
             
-            createRoom : function() {
-                
-                this.controleDeSurface();
-                
-                if (! this.form.valid) {
-                    $.notify(this.form.msg);
-                }else{
-                    var parametres = {'room': GLOBAL.token, 'nbUsersMax': this.param.user, 'nbQuestions' : this.param.question, 'timerQuestion' : this.param.timer};
-                    socket.emit('param-room', parametres , function (data) {
-                        if (data["url"] != null){
-                            document.location=data["url"];
-                        }
-                    });
-                }
-            },
+            this.controleDeSurface();
             
-            afficherDiv : function(step) {
-                this.state.step = step; 
-                $(".avant").addClass("afficherVerso");
-                $(".arriere").addClass("afficherRecto");
-            },
-            
-            retourEcran : function() {
-                $(".arriere").attr("class","arriere");
-                $(".avant").attr("class","avant");
+            if (! this.form.valid) {
+                $.notify(this.form.msg);
+            }else{
+                var parametres = {'room': GLOBAL.token, 'nbUsersMax': this.param.user, 'nbQuestions' : this.param.question, 'timerQuestion' : this.param.timer};
+                socket.emit('param-room', parametres , function (data) {
+                    if (data["url"] != null){
+                        document.location=data["url"];
+                    }
+                });
             }
+        },
+        
+        afficherDiv : function(step) {
+            this.state.step = step; 
+            $(".avant").addClass("afficherVerso");
+            $(".arriere").addClass("afficherRecto");
+        },
+        
+        retourEcran : function() {
+            $(".arriere").attr("class","arriere");
+            $(".avant").attr("class","avant");
         }
-   
+    }
 });
