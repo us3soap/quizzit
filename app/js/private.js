ff135c0e536ff1ca614d22190785b3c82a4434e9
Kevin V <kevin.valette@gmail.com>
2016-10-04 15:52:07 +0000

    ajout de l'écran de paramétrage

--- app/js/private.js
+++ app/js/private.js
@@ -1,15 +1,45 @@
 /* global Vue*/
 /* global $*/
+/* global document*/
 'use strict';
+
+const routes = {
+  '': 'dashboard',
+  '#dashboard': 'dashboard',
+  '#user': 'user'
+}
+
 new Vue({
     el: '#content',
     data: {
-
+        user: {},
+        rooms : {},
+        currentView: 'dashboard'
+    },
+    components: {
+        dashboard: { template: '<div>A custom dashboard!</div>' },
+        user: { template: '<div>A custom user!</div>' }
     },
     ready: function() {
         $('#content').show();
+        
+        this.user = { firstname : 'John', name : 'Donald.'};
+        
+        if(! this.user.firstname){
+            document.location = '/private/login';
+        }
+        this.currentView = routes[window.location.hash];
     },
     methods: {
-
+        navigate : function(){
+            this.currentView = routes[window.location.hash];
+        },
+        
+        disconnect : function(){
+            document.location = '/private/login';
+        }
+        
     }
-});
+})
+
+
