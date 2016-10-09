eef7ce494536c0611243ea65d510acce65895d6a
us3soap <kevin.valette@gmail.com>
2016-10-09 14:38:22 +0200

    correction bower

--- gulp/config/compile/bower.js
+++ gulp/config/compile/bower.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var minify = require('gulp-minify');
+var gulpFilter = require('gulp-filter');
 
 gulp.task('bower', function() {
+    var filterJS = gulpFilter('**/*.min.js', { restore: true });
     return gulp.src(mainBowerFiles(), { base: 'bower_components' })
-        .pipe(minify())
         .pipe(gulp.dest('dist/bower_components'))
 });
--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 //temporarily disabling jshint
-gulp.task('build', ['html', 'images', 'bower', 'compress', 'extras', 'wiredep'], function () {
+gulp.task('build', ['html', 'images', 'bower', 'compress', 'extras'], function () {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
